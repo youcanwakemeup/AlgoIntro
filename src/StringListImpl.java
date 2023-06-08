f1d28af40482e93288a0d328c53671e4331a94ab
shard <alisher.sharipov2@mail.ru>
2023-06-09 02:56:59 +0600

    initial push

---
+++ src/StringListImpl.java
@@ -0,0 +1,71 @@
+public class StringListImpl implements StringList {
+    @Override
+    public String add(String item) {
+        return null;
+    }
+
+    @Override
+    public String add(int index, String item) {
+        return null;
+    }
+
+    @Override
+    public String set(int index, String item) {
+        return null;
+    }
+
+    @Override
+    public String remove(String item) {
+        return null;
+    }
+
+    @Override
+    public String remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(String item) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(String item) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(String item) {
+        return 0;
+    }
+
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean equals(StringList otherList) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public String[] toArray() {
+        return new String[0];
+    }
+}
